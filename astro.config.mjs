0b8c2185cb2c644cacb4400dfa309220b2985642
kinesis <kinesis@kashic.dev>
2025-12-30 12:48:12 +0900

    :hammer: Fix: site domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://Team-ROBIT.github.io',
+    site: 'https://team-robit.github.io',
 
     vite: {
     plugins: [tailwindcss()],
